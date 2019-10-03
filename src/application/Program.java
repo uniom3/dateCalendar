02d09ed31e389ad58910c072bc24d8180c9f9de5
uniom3 <SDeivid.Mendonca@intranet.local>
2019-10-03 10:19:01 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Program {
+
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");	
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		
+		System.out.println(sdf.format(d));
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
+		System.out.println(sdf.format(d));
+		
+		int min = cal.get(Calendar.MINUTE);
+		int mon = 1+ cal.get(Calendar.MONTH);
+		System.out.println((min));
+		System.out.println((mon));
+	}
+
+}
